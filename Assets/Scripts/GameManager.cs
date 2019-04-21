f815222e5e1a027bd04f79da14d606ec579784df
본인 GitHub 아이디[출처] <qead753951@naver.com>
2019-04-21 16:17:17 +0900

    add Sprites

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private static GameManager instance;
+    public static GameManager GetInstance
+    {
+        get { return instance; }
+    }
+    public Sprite[] sprites;
+    public string spritePath;
+    void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        sprites = Resources.LoadAll<Sprite>(spritePath);
+
+    }
+    public GameObject leftTouch;
+    public GameObject rightTouch;
+    public GameObject imageObj;
+    public Image img;
+
     void Start()
     {
-        
+        ChangeSprite();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void ChangeSprite()
     {
-        
+        imageObj.GetComponent<SpriteRenderer>().sprite = sprites[1];
     }
+
+
+
+
 }
---
+++ Assets/Scripts/Touch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Touch : MonoBehaviour//, IPointerDownHandler, IPointerUpHandler
+{
+    /*private bool isBtnDown = false;
+
+    private void Update()
+    {
+        if (isBtnDown)
+        {
+            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isBtnDown = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isBtnDown = false;
+    }*/
+
+
+
+
+
+
+}
